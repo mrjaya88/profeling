525b22d334bd0cbc2dfe374d3d0bb6c09444ad9b
mrjaya88 <138643209+mrjaya88@users.noreply.github.com>
2023-07-06 18:52:40 +0700

    Update Menu.py

--- Core/Support/Menu.py
+++ Core/Support/Menu.py
@@ -1,6 +1,6 @@
-# ORIGINAL CREATOR: Luca Garofalo (Lucksi)
-# AUTHOR: Luca Garofalo (Lucksi)
-# Copyright (C) 2021-2023 Lucksi <lukege287@gmail.com>
+# ORIGINAL CREATOR: mrsisec
+# AUTHOR: mrsisec
+# Copyright (C) 2021-2023 Lucksi <sisecdev@gmail.com>
 # License: GNU General Public License v3.0
 
 import os
